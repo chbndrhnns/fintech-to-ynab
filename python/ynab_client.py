a0a1a2fcb18d704e1619372fa9cc3f1af61fe7ca
chbndrhnns <chbndrhnns@users.noreply.github.com>
2018-01-09 11:37:13 +0100

    after adding a payee locally, it needs to be added to the local cache, as well, to avoid duplicates

--- python/ynab_client.py
+++ python/ynab_client.py
@@ -7,12 +7,16 @@ from sqlalchemy.sql.expression import exists
 
 client = None
 
+
 def init():
     global client
-    client = nYnabClient(email=settings.ynab_username, password=settings.ynab_password, budgetname=settings.ynab_budget, logger=settings.log)
+    client = nYnabClient(email=settings.ynab_username, password=settings.ynab_password, budgetname=settings.ynab_budget,
+                         logger=settings.log)
+
 
 accounts = {}
 payees = {}
+
 
 def sync():
     settings.log.debug('syncing')
@@ -20,15 +24,18 @@ def sync():
     cache_accounts()
     cache_payees()
 
+
 def cache_accounts():
     global accounts
     settings.log.debug('assigning accounts')
     accounts = {x.account_name: x for x in client.budget.be_accounts}
 
+
 def cache_payees():
     global payees
     settings.log.debug('assigning payees')
     payees = {p.name: p for p in client.budget.be_payees}
+
 
 def getaccount(accountname):
     try:
@@ -38,11 +45,13 @@ def getaccount(accountname):
         settings.log.error('Couldn''t find this account: %s' % accountname)
         return False
 
+
 def payeeexists(payeename):
     try:
         return payees[payeename]
     except KeyError:
         return False
+
 
 def getpayee(payeename):
     try:
@@ -50,21 +59,24 @@ def getpayee(payeename):
         return payees[payeename]
     except KeyError:
         settings.log.debug('Couldn''t find this payee: %s' % payeename)
-        payee=Payee(name=payeename)
+        payee = Payee(name=payeename)
         client.budget.be_payees.append(payee)
+        cache_payees()
         return payee
 
+
 def containsDuplicate(transaction):
-    return client.session.query(exists()\
-   	.where(Transaction.amount==transaction.amount)\
-      	.where(Transaction.entities_account_id==transaction.entities_account_id)\
-      	.where(Transaction.date==transaction.date.date())\
-        .where(Transaction.imported_payee==transaction.imported_payee)\
-        .where(Transaction.source==transaction.source)\
-        ).scalar()
+    return client.session.query(exists()
+                                .where(Transaction.amount == transaction.amount)
+                                .where(Transaction.entities_account_id == transaction.entities_account_id)
+                                .where(Transaction.date == transaction.date.date())
+                                .where(Transaction.imported_payee == transaction.imported_payee)
+                                .where(Transaction.source == transaction.source)
+                                ).scalar()
+
 
 def findPreviousTransaction(payee_name):
-    return client.session.query(Transaction)\
-        .filter(Transaction.imported_payee==payee_name)\
-        .order_by(Transaction.date.desc())\
+    return client.session.query(Transaction) \
+        .filter(Transaction.imported_payee == payee_name) \
+        .order_by(Transaction.date.desc()) \
         .first()
